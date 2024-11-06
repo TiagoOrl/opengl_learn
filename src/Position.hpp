77beb33407d00d5a6b84911f1e1de27d1fd95240
Tiago Orlando <tiago_orlando@yahoo.com>
2024-11-05 21:30:14 -0300

    feat: added rotation and position class

---
+++ src/Position.hpp
@@ -0,0 +1,25 @@
+#ifndef H_POSITION_CLASS
+#define H_POSITION_CLASS
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+
+class Position {
+    public:
+        glm::mat4 trans;
+        Position(GLfloat posX, GLfloat posY);
+        void moveHorizontal(GLfloat posX);
+        void moveVertical(GLfloat posY);
+        void rotate(GLfloat angle);
+
+    private:
+        GLfloat posX;
+        GLfloat posY;
+        GLfloat rotationZ;
+        void translate();
+};
+
+
+#endif
