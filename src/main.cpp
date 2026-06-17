84b660f1a305ba4c8d6d6c4f37880579b42b0115
Tiago <tiago_orlando@yahoo.com>
2026-06-17 15:18:56 -0300

    identation

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include <math.h>
 
 #include "./object/Object.hpp"
 #include "./object/Light.hpp"
